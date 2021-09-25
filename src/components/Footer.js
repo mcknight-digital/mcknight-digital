ea52561410d9bedfd3bfe826e9ae57f23f05a946
Adam McKnight <adammcknight@me.com>
2021-09-25 19:50:12 -0400

    Updated the dba name in the footer componenet.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <div className="Footer">
         <h1>&copy; 2021 - MCKNIGHT DIGITAL</h1>
-        <h3>ALL RIGHTS RESERVED &mdash; Adam McKnight d.b.a, McKnight Digital & IT Solutions</h3>
+        <h3>ALL RIGHTS RESERVED &mdash; Adam McKnight d.b.a, The McKnight Organization</h3>
         <h3>Disclaimer: All thoughts and opinions are in no way are influenced or represented by the Company or Organizations we may represent.</h3>
         <h3 style={{marginBottom: "30px"}}>Built with React, Proudly Deployed on &nbsp;
         
